5b60d018c7d9da3a09c4876eff8e2f344fede4be
Ben Fitzhardinge <ben@fitzhardinge.net>
2023-02-28 17:44:23 +0800

    added redirect bucket

---
+++ infrastructure/pythonwa.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import aws_cdk as cdk
+
+from redirect.redirect import RedirectStack
+
+AWS_ACCOUNT = '155122333172'
+AWS_REGION = 'ap-southeast-2'
+SITE_DOMAIN_NAME = 'pythonwa.com'
+DOMAIN_CERTIFICATE_ARN = 'arn:aws:acm:us-east-1:155122333172:certificate/3bba3daa-5daf-4c47-9ce1-248feb820921'
+HOSTED_ZONE_ID = 'Z03934201M8O7YD4DTYVA'
+HOSTED_ZONE_NAME = 'pythonwa.com'
+
+app = cdk.App()
+RedirectStack(app, "RedirectStack",
+              site_domain_name=SITE_DOMAIN_NAME,
+              domain_certificate_arn=DOMAIN_CERTIFICATE_ARN,
+              hosted_zone_id=HOSTED_ZONE_ID,
+              hosted_zone_name=HOSTED_ZONE_NAME,
+
+              # If you don't specify 'env', this stack will be environment-agnostic.
+              # Account/Region-dependent features and context lookups will not work,
+              # but a single synthesized template can be deployed anywhere.
+
+              # Uncomment the next line to specialize this stack for the AWS Account
+              # and Region that are implied by the current CLI configuration.
+
+              # env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+
+              # Uncomment the next line if you know exactly what Account and Region you
+              # want to deploy the stack to. */
+
+              env=cdk.Environment(account=AWS_ACCOUNT, region=AWS_REGION),
+
+              # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+              )
+
+app.synth()
---
+++ infrastructure/redirect/redirect.py
@@ -0,0 +1,42 @@
+from aws_cdk import (
+    # Duration,
+    Stack,
+    # aws_sqs as sqs,
+    aws_s3
+)
+from constructs import Construct
+
+from static_site import StaticSitePublicS3
+
+
+
+class RedirectStack(Stack):
+
+    def __init__(
+            self,
+            scope: Construct,
+            construct_id: str,
+            site_domain_name: str,
+            domain_certificate_arn: str,
+            hosted_zone_id: str,
+            hosted_zone_name: str,
+            **kwargs
+    ) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # The code that defines your stack goes here
+
+        # example resource
+        # queue = sqs.Queue(
+        #     self, "TempQueue",
+        #     visibility_timeout=Duration.seconds(300),
+        # )
+        site = StaticSitePublicS3(
+            self,
+            f"{site_domain_name}-construct",
+            site_domain_name=site_domain_name,
+            domain_certificate_arn=domain_certificate_arn,
+            origin_referer_header_parameter_name="/prod/static-site/origin-custom-header/referer",
+            hosted_zone_id=hosted_zone_id,
+            hosted_zone_name=hosted_zone_name,
+        )
