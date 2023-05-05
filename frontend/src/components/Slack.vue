b49b1d94b0d16bbb23b52bb7c266d76644fa3a61
Ben Fitzhardinge <ben@fitzhardinge.net>
2023-05-05 11:36:40 +0800

    WIP: fetch code for the slack api

---
+++ frontend/src/components/Slack.vue
@@ -0,0 +1,21 @@
+<script setup>
+const email = "getemail@from.somewhere"
+const options = {
+  method: 'POST',
+  headers: {'Content-Type': 'application/json', 'x-api-key': `${import.meta.env.VITE_SLACK_API_KEY}`},
+  body: `{"email":"${email}"}`
+};
+
+fetch(`${import.meta.env.VITE_SLACK_API_ENDPOINT}`, options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
